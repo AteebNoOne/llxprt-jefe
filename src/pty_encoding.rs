a03e8a311208070b6b32ff53d1b4c42dcc849a0d
AteebNoOne <ateebnoone@gmail.com>
2026-07-06 19:49:44 +0500

    feat: preserve modifier-aware CSI encoding for non-character PTY keys (fixes #19)

--- src/pty_encoding.rs
+++ src/pty_encoding.rs
@@ -24,22 +24,53 @@ pub fn ctrl_char_to_byte(c: char) -> Option<u8> {
     }
 }
 
-fn function_key_to_bytes(n: u8) -> Option<Vec<u8>> {
-    Some(match n {
-        1 => b"\x1bOP".to_vec(),
-        2 => b"\x1bOQ".to_vec(),
-        3 => b"\x1bOR".to_vec(),
-        4 => b"\x1bOS".to_vec(),
-        5 => b"\x1b[15~".to_vec(),
-        6 => b"\x1b[17~".to_vec(),
-        7 => b"\x1b[18~".to_vec(),
-        8 => b"\x1b[19~".to_vec(),
-        9 => b"\x1b[20~".to_vec(),
-        10 => b"\x1b[21~".to_vec(),
-        11 => b"\x1b[23~".to_vec(),
-        12 => b"\x1b[24~".to_vec(),
-        _ => return None,
-    })
+fn modifiers_to_param(modifiers: KeyModifiers) -> Option<u8> {
+    let shift = if modifiers.contains(KeyModifiers::SHIFT) { 1 } else { 0 };
+    let alt = if modifiers.contains(KeyModifiers::ALT) { 2 } else { 0 };
+    let ctrl = if modifiers.contains(KeyModifiers::CONTROL) { 4 } else { 0 };
+    let meta = if modifiers.contains(KeyModifiers::META) || modifiers.contains(KeyModifiers::SUPER) { 8 } else { 0 };
+    let val = 1 + shift + alt + ctrl + meta;
+    if val > 1 {
+        Some(val)
+    } else {
+        None
+    }
+}
+
+fn function_key_to_bytes(n: u8, modifier: Option<u8>) -> Option<Vec<u8>> {
+    if let Some(param) = modifier {
+        Some(match n {
+            1 => format!("\x1b[1;{param}P").into_bytes(),
+            2 => format!("\x1b[1;{param}Q").into_bytes(),
+            3 => format!("\x1b[1;{param}R").into_bytes(),
+            4 => format!("\x1b[1;{param}S").into_bytes(),
+            5 => format!("\x1b[15;{param}~").into_bytes(),
+            6 => format!("\x1b[17;{param}~").into_bytes(),
+            7 => format!("\x1b[18;{param}~").into_bytes(),
+            8 => format!("\x1b[19;{param}~").into_bytes(),
+            9 => format!("\x1b[20;{param}~").into_bytes(),
+            10 => format!("\x1b[21;{param}~").into_bytes(),
+            11 => format!("\x1b[23;{param}~").into_bytes(),
+            12 => format!("\x1b[24;{param}~").into_bytes(),
+            _ => return None,
+        })
+    } else {
+        Some(match n {
+            1 => b"\x1bOP".to_vec(),
+            2 => b"\x1bOQ".to_vec(),
+            3 => b"\x1bOR".to_vec(),
+            4 => b"\x1bOS".to_vec(),
+            5 => b"\x1b[15~".to_vec(),
+            6 => b"\x1b[17~".to_vec(),
+            7 => b"\x1b[18~".to_vec(),
+            8 => b"\x1b[19~".to_vec(),
+            9 => b"\x1b[20~".to_vec(),
+            10 => b"\x1b[21~".to_vec(),
+            11 => b"\x1b[23~".to_vec(),
+            12 => b"\x1b[24~".to_vec(),
+            _ => return None,
+        })
+    }
 }
 
 /// Convert a key event to raw bytes for PTY input.
@@ -86,17 +117,93 @@ pub fn key_to_bytes(key: &KeyEvent, passthrough_enter: bool) -> Option<Vec<u8>> 
         KeyCode::Backspace => vec![0x7f],
         KeyCode::Tab => vec![b'\t'],
         KeyCode::Esc => vec![0x1b],
-        KeyCode::Up => b"\x1b[A".to_vec(),
-        KeyCode::Down => b"\x1b[B".to_vec(),
-        KeyCode::Right => b"\x1b[C".to_vec(),
-        KeyCode::Left => b"\x1b[D".to_vec(),
-        KeyCode::Home => b"\x1b[H".to_vec(),
-        KeyCode::End => b"\x1b[F".to_vec(),
-        KeyCode::PageUp => b"\x1b[5~".to_vec(),
-        KeyCode::PageDown => b"\x1b[6~".to_vec(),
-        KeyCode::Delete => b"\x1b[3~".to_vec(),
-        KeyCode::Insert => b"\x1b[2~".to_vec(),
-        KeyCode::F(n) => function_key_to_bytes(n)?,
+        KeyCode::Up => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}A").into_bytes()
+            } else {
+                b"\x1b[A".to_vec()
+            }
+        }
+        KeyCode::Down => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}B").into_bytes()
+            } else {
+                b"\x1b[B".to_vec()
+            }
+        }
+        KeyCode::Right => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}C").into_bytes()
+            } else {
+                b"\x1b[C".to_vec()
+            }
+        }
+        KeyCode::Left => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}D").into_bytes()
+            } else {
+                b"\x1b[D".to_vec()
+            }
+        }
+        KeyCode::Home => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}H").into_bytes()
+            } else {
+                b"\x1b[H".to_vec()
+            }
+        }
+        KeyCode::End => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[1;{param}F").into_bytes()
+            } else {
+                b"\x1b[F".to_vec()
+            }
+        }
+        KeyCode::PageUp => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[5;{param}~").into_bytes()
+            } else {
+                b"\x1b[5~".to_vec()
+            }
+        }
+        KeyCode::PageDown => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[6;{param}~").into_bytes()
+            } else {
+                b"\x1b[6~".to_vec()
+            }
+        }
+        KeyCode::Delete => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[3;{param}~").into_bytes()
+            } else {
+                b"\x1b[3~".to_vec()
+            }
+        }
+        KeyCode::Insert => {
+            if let Some(param) = modifiers_to_param(key.modifiers) {
+                alt_encoded = true;
+                format!("\x1b[2;{param}~").into_bytes()
+            } else {
+                b"\x1b[2~".to_vec()
+            }
+        }
+        KeyCode::F(n) => {
+            let param = modifiers_to_param(key.modifiers);
+            if param.is_some() {
+                alt_encoded = true;
+            }
+            function_key_to_bytes(n, param)?
+        }
         _ => return None,
     };
 
@@ -330,6 +437,62 @@ mod key_tests {
         assert_eq!(key_to_bytes(&f2, false), Some(b"\x1bOQ".to_vec()));
         assert_eq!(key_to_bytes(&f12, false), Some(b"\x1b[24~".to_vec()));
         assert_ne!(key_to_bytes(&f2, false), key_to_bytes(&insert, false));
+    }
+
+    #[test]
+    fn modified_arrow_keys_use_xterm_sequences() {
+        let ctrl_up = key_event(KeyCode::Up, KeyModifiers::CONTROL);
+        let alt_down = key_event(KeyCode::Down, KeyModifiers::ALT);
+        let shift_right = key_event(KeyCode::Right, KeyModifiers::SHIFT);
+        let ctrl_alt_left = key_event(KeyCode::Left, KeyModifiers::CONTROL | KeyModifiers::ALT);
+
+        // ctrl parameter = 5
+        assert_eq!(key_to_bytes(&ctrl_up, false), Some(b"\x1b[1;5A".to_vec()));
+        // alt parameter = 3
+        assert_eq!(key_to_bytes(&alt_down, false), Some(b"\x1b[1;3B".to_vec()));
+        // shift parameter = 2
+        assert_eq!(key_to_bytes(&shift_right, false), Some(b"\x1b[1;2C".to_vec()));
+        // ctrl + alt parameter = 7
+        assert_eq!(key_to_bytes(&ctrl_alt_left, false), Some(b"\x1b[1;7D".to_vec()));
+    }
+
+    #[test]
+    fn modified_edit_keys_use_xterm_sequences() {
+        let ctrl_pageup = key_event(KeyCode::PageUp, KeyModifiers::CONTROL);
+        let alt_pagedown = key_event(KeyCode::PageDown, KeyModifiers::ALT);
+        let shift_delete = key_event(KeyCode::Delete, KeyModifiers::SHIFT);
+        let ctrl_alt_insert = key_event(KeyCode::Insert, KeyModifiers::CONTROL | KeyModifiers::ALT);
+        let shift_home = key_event(KeyCode::Home, KeyModifiers::SHIFT);
+        let ctrl_end = key_event(KeyCode::End, KeyModifiers::CONTROL);
+
+        assert_eq!(key_to_bytes(&ctrl_pageup, false), Some(b"\x1b[5;5~".to_vec()));
+        assert_eq!(key_to_bytes(&alt_pagedown, false), Some(b"\x1b[6;3~".to_vec()));
+        assert_eq!(key_to_bytes(&shift_delete, false), Some(b"\x1b[3;2~".to_vec()));
+        assert_eq!(key_to_bytes(&ctrl_alt_insert, false), Some(b"\x1b[2;7~".to_vec()));
+        assert_eq!(key_to_bytes(&shift_home, false), Some(b"\x1b[1;2H".to_vec()));
+        assert_eq!(key_to_bytes(&ctrl_end, false), Some(b"\x1b[1;5F".to_vec()));
+    }
+
+    #[test]
+    fn modified_function_keys_use_xterm_sequences() {
+        let ctrl_f1 = key_event(KeyCode::F(1), KeyModifiers::CONTROL);
+        let alt_f5 = key_event(KeyCode::F(5), KeyModifiers::ALT);
+        let ctrl_alt_f12 = key_event(KeyCode::F(12), KeyModifiers::CONTROL | KeyModifiers::ALT);
+
+        assert_eq!(key_to_bytes(&ctrl_f1, false), Some(b"\x1b[1;5P".to_vec()));
+        assert_eq!(key_to_bytes(&alt_f5, false), Some(b"\x1b[15;3~".to_vec()));
+        assert_eq!(key_to_bytes(&ctrl_alt_f12, false), Some(b"\x1b[24;7~".to_vec()));
+    }
+
+    #[test]
+    fn alt_encoding_is_consistent_and_not_double_encoded() {
+        // Alt-up modified should be \x1b[1;3A, not double ESC-prefixed (e.g. not \x1b\x1b[1;3A)
+        let alt_up = key_event(KeyCode::Up, KeyModifiers::ALT);
+        assert_eq!(key_to_bytes(&alt_up, false), Some(b"\x1b[1;3A".to_vec()));
+
+        // Alt-F1 modified should be \x1b[1;3P, not \x1b\x1b[1;3P
+        let alt_f1 = key_event(KeyCode::F(1), KeyModifiers::ALT);
+        assert_eq!(key_to_bytes(&alt_f1, false), Some(b"\x1b[1;3P".to_vec()));
     }
 }
 
